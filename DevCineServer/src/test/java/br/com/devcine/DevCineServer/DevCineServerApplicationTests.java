60d31007840b1d5d98788949808ba23d415ced02
Gabriel Ricco <gabrielrdantas@outlook.com>
2023-11-16 18:02:42 -0300

    first commit

---
+++ DevCineServer/src/test/java/br/com/devcine/DevCineServer/DevCineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devcine.DevCineServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCineServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
